957348bb503f1f003a9eb027df6a272a8d4ab21c
Matthew Rougely <matthewrougelysr@gmail.com>
2020-07-21 16:10:09 -0700

    checkpoint 15 complete

--- src/app.js
+++ src/app.js
@@ -3,35 +3,18 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
-
-const { NODE_ENV, API_TOKEN } = require('./config');
-const logger = require('./logger');
-const bookmarksRouter = require('./bookmarks-list-router');
+const { NODE_ENV } = require('./config');
+const validateBearerToken = require('./validate-bearer-token');
+const errorHandler = require('./error-handler');
+const bookmarksRouter = require('./bookmarks/bookmarks-router');
 
 const app = express();
 
-const morganOption = (NODE_ENV === 'production')
-  ? 'tiny'
-  : 'common';
-
-app.use(morgan(morganOption));
-app.use(helmet());
+app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
+  skip: () => NODE_ENV === 'test'
+}));
 app.use(cors());
-
-
-
-const validateBearerToken = (req, res, next) => {
-  const apiToken = API_TOKEN;
-  const authToken = req.get('authorization');
-
-  if(!authToken || authToken.split(' ')[1] !== apiToken ) {
-    logger.error(`Unauthorized request to path: ${req.path}`);
-    return res.status(401).json({ error: 'Unauthorized request' });
-  }
-
-  next();
-};
-
+app.use(helmet());
 app.use(validateBearerToken);
 
 app.use(bookmarksRouter);
@@ -40,14 +23,6 @@ app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
 
-app.use(function errorHandler(error, req, res, next) { 
-  let response;
-  if (NODE_ENV === 'production') {
-    response = { error: { message: 'Internal server error' } };
-  } else {
-    response = { message: error.message, error }
-  }
-  res.status(500).json(response);
-});
+app.use(errorHandler);
 
 module.exports = app;
